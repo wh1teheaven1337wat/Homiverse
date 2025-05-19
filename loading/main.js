9db58d9e2bc209a3eff1ee158122b08a8608d0d0
Bduidan <85570800+Alex9600t@users.noreply.github.com>
2025-05-19 04:14:56 +0000

    feat(test): Added music

---
+++ loading/main.js
@@ -0,0 +1,9 @@
+const audio = document.querySelector(".player");
+const l = document.querySelector(".l");
+l.addEventListener("click", (event) => {
+  audio.play()
+});
+// audio.play();
+window.onload = function() {
+    audio.play()
+}
