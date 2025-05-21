730586b17021b113f5a47c99ada70218fc469395
Bduidan <85570800+Alex9600t@users.noreply.github.com>
2025-05-21 18:37:53 +0000

    fix(IDK)

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 document.querySelector(".pashalka").addEventListener("click", (event) => {
-    document.body.innerHTML = '<img style="width: 100vw; height: 100vh;" src="img/soggycat.webp" alt=""><audio autoplay src="../img/pashalco.mp3" class="player"></audio>'
+    document.body.innerHTML = '<img style="width: 100vw; height: 100vh;" src="img/soggycat.webp" alt=""><audio autoplay src="https://wh1teheaven1337wat.github.io/Homiverse/img/pashalco.mp3" class="player"></audio>'
 });
 document.querySelector(".pashalka2").addEventListener("click", (event) => {
-    document.body.innerHTML = '<img style="width: 100vw; height: 100vh;" src="img/soggycat.webp" alt=""><audio autoplay src="../img/pashalco.mp3" class="player"></audio>'
+    document.body.innerHTML = '<img style="width: 100vw; height: 100vh;" src="img/soggycat.webp" alt=""><audio autoplay src="https://wh1teheaven1337wat.github.io/Homiverse/img/pashalco.mp3" class="player"></audio>'
 });
 document.querySelector(".pashalka3").addEventListener("click", (event) => {
-    document.body.innerHTML = '<img style="width: 100vw; height: 100vh;" src="img/soggycat.webp" alt=""><audio autoplay src="../img/pashalco.mp3" class="player"></audio>'
+    document.body.innerHTML = '<img style="width: 100vw; height: 100vh;" src="img/soggycat.webp" alt=""><audio autoplay src="https://wh1teheaven1337wat.github.io/Homiverse/img/pashalco.mp3" class="player"></audio>'
 });
