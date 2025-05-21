5422de2ca31ebca84f545eebf797c4dcc758bf9d
Bduidan <85570800+Alex9600t@users.noreply.github.com>
2025-05-21 20:56:36 +0000

    feat(Button): Added new button

--- js/index.js
+++ js/index.js
@@ -7,3 +7,14 @@ document.querySelector(".pashalka2").addEventListener("click", (event) => {
 document.querySelector(".pashalka3").addEventListener("click", (event) => {
     document.body.innerHTML = '<img style="width: 100vw; height: 100vh;" src="img/soggycat.webp" alt=""><audio autoplay src="https://wh1teheaven1337wat.github.io/Homiverse/img/pashalco.mp3" class="player"></audio>'
 });
+
+
+window.fetch('https://api.gamemonitoring.ru/servers/9287429')
+.then((response) => response.json())
+.then((data) => document.querySelector(".online").textContent = data.response.numplayers)
+
+setInterval(() => {
+    window.fetch('https://api.gamemonitoring.ru/servers/9287429')
+.then((response) => response.json())
+.then((data) => document.querySelector(".online").textContent = data.response.numplayers)
+}, 30000)
